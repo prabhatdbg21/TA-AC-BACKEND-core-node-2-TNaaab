19609de2ed0ce15127d9ac47e564f663b733457a
Prabhat <prabhatdbg21@gmail.com>
2023-02-13 09:32:47 +0530

    complited

--- block-BNaabb/stream.js
+++ block-BNaabb/stream.js
@@ -4,16 +4,16 @@ var http = require ('http');
 var server = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
-    if(req.method === 'POST' && req.url === '/') {
-        var store = '';
-        req.on('data', (chunk) => {
-            store = store + chunk;
-        });
-        req.on('end', () => {   
+    var store = '';
+    req.on('data', (chunk) => {
+        store = store + chunk;
+    });
+    req.on('end', () => {  
+        if(req.method === 'POST' && req.url === '/') {
             res.write(store) ;
             res.end() ;
-        })
-    }
+        } 
+    })
 }
 
 server.listen(3456, () => {
